a0e951c91a8e775b91c46e3f7b3358c3914b7416
Yakov Budnikov <dizertir64@gmail.com>
2019-11-12 21:16:18 +0300

    создание классов буффера и сервера

--- Code.py
+++ Code.py
@@ -1,7 +1,7 @@
 import numpy as np
-
+import random
 # линейный распределенный закон для T = (Tmax - Tmin) * x[i] - Tmin
-def getTime(Tmin = 1, Tmax = 5, randNum = 1.0):
+def getTime(Tmin = 1, Tmax = 5, randNum = random.random()):
     T = (Tmax - Tmin) * randNum - Tmin
     return T
 
@@ -12,10 +12,60 @@ def getRandNum(xi = 1.0, a = 17, M = 1000, b = 1):
     return xi, rand
 
 
+def mainLoop(Tzmin, Tzmax, Tsmin, Tsmax, a, xi, M, b): 
+    xi, randNum = getRandNum(xi, a, M, b)
+    T = getTime(Tzmin, Tzmin, randNum)
+    
+    pass 
+class Buffer(object):
+    def __init__(self, size = 3):
+        self.bufferSize = size
+        self.storage = list(range(size))
+        for i in range():
+            self.storage[i] = 0
+
+    def isEmpty(self):
+        for i in range(self.storage):
+            if self.storage[i] != 0:
+                return False
+        return True
+                
+
+class Server(object):
+    def __init__(self):
+        self.busy = False
+        self.OperationTime = 0
+
+    def isBusy(self):
+        return self.busy
+    
+    def getOperationTime(self):
+        return self.OperationTime
+
+    def SetBusyServer(self, busy):
+        self.busy = busy
+
+    def OperationTimeSet(self, T):
+        self.OperationTime = T    
+
 if __name__ == "__main__":
     
     Server1 = True
     Server2 = True
+    
+    print ("Время прихода программ (линейный закон)")
+    Tzmin = float(input("Tzmin = "))
+    Tzmax = float(input("Tzmax = "))
+    
+    print ("Время обработки задач (линейный закон)")
+    Tsmin = int(input("Tsmin = "))
+    Tsmax = int(input("Tsmax = "))
 
+    print ("Настройка рандомайзера")
+    a = int(input("a = "))
+    x0 = int(input("x0 = "))
+    M = int(input("M = "))
+    b = int(input("b = "))
 
+    mainLoop(Tzmin, Tzmax, Tsmin, Tsmax, a, x0, M, b)
     pass
