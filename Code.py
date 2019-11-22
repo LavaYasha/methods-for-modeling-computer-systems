60ca06c2cebdc0db0de515046785641cb8029b20
Yakov Budnikov <dizertir64@gmail.com>
2019-11-22 19:01:45 +0300

    add buffer

--- Code.py
+++ Code.py
@@ -1,7 +1,5 @@
-import numpy as np
 import random
-from itertools import accumulate
-import os
+
 # линейный распределенный закон для T = (Tmax - Tmin) * x[i] - Tmin
 def getTime(Tmin, Tmax, randNum):
     "linear distribution law for time"
@@ -23,30 +21,30 @@ class Server(object):
         self.CurrentOperationTime = 0 
         self.downtime = 0
         self.workTime = 0
-        self.pocCount = 0
+        self.procCount = 0
 
-def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount):
-    "собственно сама функция расчета"
+def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, bufferSize):
+    "функция расчета"
+    #   время прихода программ
     TimeIn = []
     
-    #   обработка по времени первой программы
+    #   время обработки программ
     WorkTime = []
     
-    TimeIn.append(round(getTime(Tzmin, Tzmax, random.random()),3))
-    WorkTime.append(round(getTime(Tsmin, Tsmax, random.random()),3))
+    TimeIn.append(round(getTime(Tzmin, Tzmax, random.random()), 3))
     
     while TimeIn[-1] < simulation_time:
         TimeIn.append(round(getTime(Tzmin, Tzmax, random.random()) + TimeIn[-1], 3))
-        WorkTime.append(round(getTime(Tsmin, Tsmax, random.random())))
-    #print ("\n\n\n")
-    #print (TimeIn)
-    #print ("\n\n\n")
+     
+
     Current_time = TimeIn[0]
     #   Настройки серверов
     Servers = []
     for k in range(ServersCount):
         Servers.append(Server())
-        
+
+    buffer = [0 for i in range(bufferSize)]
+
     #   основной цикл внутри симмуляции
     for i in range(len(TimeIn)):
         
@@ -59,14 +57,19 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount):
                 emptyServer = j 
                 break
         
-        if isOneServerFree:
-            pass
+        if isOneServerFree and emptyServer != -1:
+            if False:
+                #TODO buffer check
+                pass
+            else:
+                Servers[emptyServer].EmploymentStatus = 'busy'
+                Servers[emptyServer].workTime = WorkTime[i]
+                
         pass
     pass
 
 
 if __name__ == "__main__":
-    os.system("CLS")
     #   время симуляции впоследствии можно будет задать в часовом диапозоне
     SimulationTime = 60 * 60 
 
@@ -78,5 +81,5 @@ if __name__ == "__main__":
     Tsmin = 1
     Tsmax = 5
 
-    colculation(SimulationTime, Tzmin, Tzmax, Tsmin, Tsmax, 2)
+    colculation(SimulationTime, Tzmin, Tzmax, Tsmin, Tsmax, 2, 3)
     pass
