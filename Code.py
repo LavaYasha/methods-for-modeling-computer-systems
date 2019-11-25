e372ab22216861674073fee88b9587bc7525b40c
Yakov Budnikov <dizertir64@gmail.com>
2019-11-25 13:58:52 +0300

    промежуточный вариант

--- Code.py
+++ Code.py
@@ -19,9 +19,19 @@ class Server(object):
     def __init__(self):
         self.EmploymentStatus = 'free' # free - свободен busy - занят
         self.CurrentOperationTime = 0 
-        self.downtime = 0
+        self.downTime = 0
         self.workTime = 0
         self.procCount = 0
+        self.InTime = 0
+        self.OutTime = 0
+
+class Buff(object):
+    def __init__(self):
+        self.TimeIn = 0
+        self.TimeOut = 0
+        self.WorkTime = 0
+        self.BufferTime = 0
+
 
 def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, bufferSize):
     "функция расчета"
@@ -32,19 +42,29 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, buffe
     WorkTime = []
     
     TimeIn.append(round(getTime(Tzmin, Tzmax, random.random()), 3))
+    WorkTime.append(round(getTime(Tsmin, Tsmax, random.random()), 3))
     
     while TimeIn[-1] < simulation_time:
         TimeIn.append(round(getTime(Tzmin, Tzmax, random.random()) + TimeIn[-1], 3))
+        WorkTime.append(round(getTime(Tsmin, Tsmax, random.random()), 3))
      
-
+    #   Время для расчетов
     Current_time = TimeIn[0]
-    #   Настройки серверов
+
+    #   Инициализация серверов
     Servers = []
     for k in range(ServersCount):
         Servers.append(Server())
+        Servers[k].downTime += Current_time
 
-    buffer = [0 for i in range(bufferSize)]
+    #  Инициализация буфера
+    buffer = [Buff() for i in range(bufferSize)]
 
+    #   Флаг работы серверов
+    isWork = False
+
+    #   Время без использования буфера
+    TimeWithoutBuffer = 0
     #   основной цикл внутри симмуляции
     for i in range(len(TimeIn)):
         
@@ -57,13 +77,33 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, buffe
                 emptyServer = j 
                 break
         
-        if isOneServerFree and emptyServer != -1:
-            if False:
-                #TODO buffer check
-                pass
-            else:
-                Servers[emptyServer].EmploymentStatus = 'busy'
-                Servers[emptyServer].workTime = WorkTime[i]
+        if (isOneServerFree) and (emptyServer != -1):
+            WorkTime.append(round(getTime(Tsmin, Tsmax, random.random()), 3))
+            Servers[emptyServer].EmploymentStatus = 'busy'
+            Servers[emptyServer].TimeIn = TimeIn[i]
+            Servers[emptyServer].workTime = WorkTime[i]
+            Servers[emptyServer].OutTime = TimeIn[i] + WorkTime[i]
+        else:
+            isOutTimeBeforeTimeIn = False
+            for k in range(len(Servers)):
+                if Servers[k].OutTime < TimeIn:
+                    isOutTimeBeforeTimeIn = True
+                    if len(buffer) > 0:
+                        buffer.pop(0)
+            
+            if isOutTimeBeforeTimeIn == False:
+                if len(buffer) < bufferSize:
+                    buffer.append(Buff())
+                    buffer[-1].WorkTime = WorkTime[i]
+                    for k in range(len(buffer)):
+                        if k == 0:
+                            buffer[k].TimeIn = TimeIn[i]
+                            #TimeWithoutBuffer +=  TimeIn[i] - TODO - не очень понятно как отсчитывать время начала работы без буффера
+                        else:
+                            buffer[k].TimeIn = TimeIn[i]
+                            buffer[k - 1].TimeOut = buffer[k].TimeIn
+                            buffer[k - 1].BufferTime += buffer[k - 1].TimeOut - buffer[k - 1].TimeIn
+                
                 
         pass
     pass
