217e00d2d50ca42513c5ecc2fe5e35aeea5b8218
Yakov Budnikov <dizertir64@gmail.com>
2019-11-27 00:13:46 +0300

    правильный расчет вероятностей работы серверов/буферов начало реализации визуальной части

--- Code.py
+++ Code.py
@@ -1,4 +1,75 @@
 import random
+from tkinter import *
+#===============================================================
+class Window(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+        self.parent = parent        
+        self.initUI()
+
+        #self.SimulationTime = 0
+ #============тут весь визуал пишется============
+    def initUI(self):
+        #Left Frame
+        self.left_frame = LabelFrame(self)
+
+        self.SimulationTime_LabelFrame = LabelFrame(self.left_frame)
+        self.SimulationTime_label = Label(self.SimulationTime_LabelFrame, text="Время симуляции")
+        self.SimulationTime_label.pack(side=LEFT, padx=10, pady=10)
+        self.SimulationTime_Entry = Entry(self.SimulationTime_LabelFrame)
+        self.SimulationTime_Entry.bind("<Any-KeyRelease>", self.detect)
+        self.SimulationTime_Entry.pack(side=RIGHT, padx=10, pady =10)
+        self.SimulationTime_LabelFrame.pack(anchor=W, padx=10, pady=10, fill=X)
+
+        self.CountServer_LabelFrame = LabelFrame(self.left_frame)
+        self.CountServer_label = Label(self.CountServer_LabelFrame, text="Колличество серверов")
+        self.CountServer_label.pack(side=LEFT, padx=10, pady=10)
+        self.CountServer_Entry = Entry(self.CountServer_LabelFrame)
+        self.CountServer_Entry.bind("<Any-KeyRelease>", self.detect)
+        self.CountServer_Entry.pack(side=RIGHT, padx=10, pady =10)
+        self.CountServer_LabelFrame.pack(anchor=W, padx=10, pady=10, fill=X)
+
+        self.BufferSize_LabelFrame = LabelFrame(self.left_frame)
+        self.BufferSize_label = Label(self.BufferSize_LabelFrame, text="Размер буфера")
+        self.BufferSize_label.pack(side=LEFT, padx=10, pady=10)
+        self.BufferSize_Entry = Entry(self.BufferSize_LabelFrame)
+        self.BufferSize_Entry.bind("<Any-KeyRelease>", self.detect)
+        self.BufferSize_Entry.pack(side=RIGHT, padx=10, pady =10)
+        self.BufferSize_LabelFrame.pack(anchor=W, padx=10, pady=10, fill=X)
+
+        self.left_frame.pack(side=LEFT, padx=5, pady=10, fill=Y)
+        #Left Frame
+
+        #Right Frame
+        self.right_frame = LabelFrame(self)
+
+        self.BufferSize_LabelFrame = LabelFrame(self.right_frame)
+        self.BufferSize_label = Label(self.BufferSize_LabelFrame, text="Размер буфера")
+        self.BufferSize_label.pack(side=LEFT, padx=10, pady=10)
+        self.BufferSize_Entry = Entry(self.BufferSize_LabelFrame)
+        self.BufferSize_Entry.bind("<Any-KeyRelease>", self.detect)
+        self.BufferSize_Entry.pack(side=RIGHT, padx=10, pady =10)
+        self.BufferSize_LabelFrame.pack(anchor=W, padx=10, pady=10, fill=X)
+
+        self.right_frame.pack(side=RIGHT, padx=5, pady=10, fill=Y)
+        #Right Frame
+
+    def detect(self, event):
+        data = event.widget.get()
+        if not data.isdigit() and data != '':
+            result = ''
+            for i in event.widget.get():
+                if i.isdigit():
+                    result += i
+            event.widget.delete(0, END)
+            event.widget.insert(0, result)
+
+    
+    def porc(self):
+        #   время симуляции впоследствии можно будет задать в часовом диапозоне
+        pass
+#===============================================================
 
 # линейный распределенный закон для T = (Tmax - Tmin) * x[i] - Tmin
 def getTime(Tmin, Tmax, randNum):
@@ -51,9 +122,13 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, buffe
     TimeIn.append(round(getTime(Tzmin, Tzmax, random.random()), 3))
     WorkTime.append(round(getTime(Tsmin, Tsmax, random.random()), 3))
     
-    while TimeIn[-1] < simulation_time:
+    while TimeIn[-1]< simulation_time:
         TimeIn.append(round(getTime(Tzmin, Tzmax, random.random()) + TimeIn[-1], 3))
         WorkTime.append(round(getTime(Tsmin, Tsmax, random.random()), 3))
+
+    #не превышаем время симуляции TODO
+    #TimeIn.pop(-1)
+    #WorkTime.pop(-1)
 
     print(f'len = {len(TimeIn)}')
     print(TimeIn)
@@ -80,7 +155,7 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, buffe
 
     #   Время простоя всей системы
     TimeFreeSystem = 0
-
+    TimeFreeSystem_begin = 0
     #   счетчик программ не обработанные Вычислительной Системой
     leaveProg = 0
     AllServersAreFree = True
@@ -89,22 +164,29 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, buffe
     #   основной цикл внутри симмуляции    #
     #======================================#
     for i in range(len(TimeIn)):
-        
+        currentTime = TimeIn[i]
         emptyServer = -1 # if -1 then all servers are busy
         isOneServerFree = False 
         
         for j in range(len(Servers)):
             if Servers[j].EmploymentStatus == 'free':
+                if isOneServerFree == True:
+                    continue
                 emptyServer = j
                 isOneServerFree = True
-                break
+            else:
+                AllServersAreFree = False
         
-        if emptyServer != -1 and isOneServerFree:
-            
+        #   Корректный расчет времени простоя системы        
+        if AllServersAreFree == True:
+            TimeFreeSystem += TimeIn[i] - TimeFreeSystem_begin
+        
+        if emptyServer != -1 and isOneServerFree:    
+
             t = getCurrentCountWorksServer(Servers) - 1
-            #while t < getCurrentCountWorksServer(Servers):
-            sameTimeServersWork[t] += round(TimeIn[i] - sameTimeServersWork_begin[t] ,3 )
-                # t += 1
+
+            if t >= 0:
+                sameTimeServersWork[t] += round(TimeIn[i] - sameTimeServersWork_begin[t] ,3 )
 
             Servers[emptyServer].EmploymentStatus = 'busy'
             Servers[emptyServer].InTime = round(TimeIn[i] , 3)
@@ -126,21 +208,25 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, buffe
                     isOutTimeBeforeTimeIn = True
                     if len(buffer) > 0:
                         getBuff = buffer.pop(0)
+                        TimeProgInCS.append(round(TimeIn[i] - getBuff.TimeIn + getBuff.WorkTime,3))
+
+                        if len(buffer) == 0:
+                            sameTimeServersWork_begin[getCurrentCountWorksServer(Servers) - 1] = round(Servers[j].OutTime , 3)
+                        
+                        TimeBuffersWork[len(buffer)] += round(Servers[j].OutTime - TimeBuffersWork_begin[len(buffer)], 3)
+
+                        if len(buffer) > 0:
+                            TimeBuffersWork_begin[len(buffer) - 1] = round(Servers[j].OutTime, 3)
+                        else:
+                            TimeFreeSystem_begin = Servers[j].OutTime
+                            
+
                         Servers[j].InTime = round(Servers[j].OutTime, 3)
                         Servers[j].EmploymentStatus = 'busy'
                         Servers[j].workTime = round(getBuff.WorkTime , 3)
                         Servers[j].OutTime = round(Servers[j].InTime + Servers[j].workTime , 3)
                         Servers[j].procCount += 1
                         
-                        TimeProgInCS.append(round(TimeIn[i] - getBuff.TimeIn + getBuff.WorkTime,3))
-
-                        sameTimeServersWork_begin[getCurrentCountWorksServer(Servers) - 1] = round(Servers[j].OutTime , 3)
-
-                        if len(buffer) == bufferSize - 1:
-                            TimeBuffersWork_begin[len(buffer) - 2] =round( Servers[j].OutTime, 3)
-                        elif len(buffer) < bufferSize - 1:
-                            TimeBuffersWork_begin[len(buffer) - 1] = round(Servers[j].OutTime, 3)
-                            TimeBuffersWork[len(buffer)] = round(TimeIn[i] - TimeBuffersWork_begin[len(buffer)], 3)
                     else:
                         sameTimeServersWork[getCurrentCountWorksServer(Servers) - 1] += round(Servers[j].OutTime - sameTimeServersWork_begin[getCurrentCountWorksServer(Servers) - 1] , 3)
                         if busyFreeServer == False:
@@ -162,21 +248,30 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, buffe
                             Servers[j].EmploymentStatus = 'free'
                             Servers[j].ReleaseTime = Servers[j].OutTime
                             sameTimeServersWork_begin[getCurrentCountWorksServer(Servers) - 1] =round( Servers[j].OutTime, 3)
+                            if getCurrentCountWorksServer(Servers) == 0:
+                                TimeFreeSystem_begin = TimeIn[i]
 
             if isOutTimeBeforeTimeIn == False:
                 if len(buffer) < bufferSize:
+                    
                     buffer.append(Buff())
                     buffer[-1].TimeIn = TimeIn[i]
+                    buffer[-1].WorkTime = WorkTime[i]
 
                     if len(buffer) == 1:
                         TimeBuffersWork_begin[0] = round(TimeIn[i], 3)
                         sameTimeServersWork[-1] += round(TimeIn[i] - sameTimeServersWork_begin[-1], 3)
-                    else:
+                       
+                    elif len(buffer) > 1:
                         TimeBuffersWork_begin[len(buffer) - 1] = round(TimeIn[i], 3)
-                        TimeBuffersWork[len(buffer) - 2] = round(TimeIn[i] - TimeBuffersWork_begin[len(buffer) - 2], 3)
+                        TimeBuffersWork[len(buffer) - 2] += round(TimeIn[i] - TimeBuffersWork_begin[len(buffer) - 2], 3)
+                    else:
+                        for i in range(1000):
+                            print('ERROR <= ОБРАТИ ВНИМАНИЕ') # не ну а вдруг
                 else:
                     leaveProg += 1
-                    TimeBuffersWork[len(buffer) - 1] = round(TimeIn[i] - TimeBuffersWork_begin[len(buffer) - 1],3)
+                    TimeBuffersWork[len(buffer) - 1] += round(TimeIn[i] - TimeBuffersWork_begin[len(buffer) - 1],3)
+                    TimeBuffersWork_begin[len(buffer) - 1] = round(TimeIn[i], 3)
                     TimeProgInCS.append(0)
 
     #======================================#
@@ -188,7 +283,6 @@ def colculation(simulation_time, Tzmin, Tzmax, Tsmin, Tsmax, ServersCount, buffe
 
 
 if __name__ == "__main__":
-    #   время симуляции впоследствии можно будет задать в часовом диапозоне
     SimulationTime = 60 * 60 
 
     #   время прихода программы (линейный закон)
@@ -217,11 +311,11 @@ if __name__ == "__main__":
 
     p0 = round(p0 / SimulationTime,7)               #   вероятность бездействия системы во время симуляции
     p1 = round(pServer[0] / SimulationTime,3)       #   вероятность работы одного сервера 
-    p2 = round(pServer[1] / SimulationTime,3)       #   вероятность работы двух серверов
+    p2 = round(pServer[1] /SimulationTime,3)       #   вероятность работы двух серверов
     p3 = round(pBuff[0] / SimulationTime,6)         #   вероятность работы с одним 
     p4 = round(pBuff[1] / SimulationTime,6)         #                        двумя
     p5 = round(pBuff[2] / SimulationTime,6)         #                        тремя буфферами
-    
+
     DoneProc = 0
     for i in range(len(Server)):
         DoneProc += Server[i].procCount
@@ -230,28 +324,34 @@ if __name__ == "__main__":
     S = round(DoneProc / SimulationTime,3)          #   абсолютная пропускная способность – среднее число программ, обработанных в единицу времени
     Potk = round(leaveProg / SimulationTime,3)      #   вероятность отказа, т.е. того, что программа будет не обработанной
 
-    K = round(p1 + p2,3)                            #  не уверен что так вычитывается данная вероятность (среднее число занятых серверов) TODO
-    Nprog = round(len(AllProc) / len(AllProc),3)    #  тоже не очень понятно (среднее число программ в ВС)                                TODO
-    Tprog = round(sum(TimeProgInCS) / SimulationTime, 3)
-    Nbuf = 0                                        # TODO
+    K = 'TODO'                                      #  не уверен что так вычитывается данная вероятность (среднее число занятых серверов) TODO
+    Nprog = 'TODO'                                  #  тоже не очень понятно (среднее число программ в ВС)                                TODO
+    Tprog = round(sum(TimeProgInCS) / SimulationTime, 3)    # Debug
+    Nbuf = 'TODO'                                           # TODO
 
     AllTimeInBuffer = sum(pBuff)
     Tbuf =round( AllTimeInBuffer / SimulationTime,3)
 
+    outPutText = (f' p0 = {p0}\n' +
+                f'p1 = {p1}\n' + 
+                f'p2 = {p2}\n' +
+                f'p3 = {p3}\n' + 
+                f'p4 = {p4}\n' + 
+                f'p5 = {p5}\n' + 
+                f'sum P = {p1 + p2 + p3 + p4 + p5}\n' + 
+                f'Q = {Q}\n' + 
+                f'S = {S}\n' + 
+                f'P отк = {Potk}\n' + 
+                f'K = {K}\n' + 
+                f'N прог = {Nprog}\n' +
+                f'T прог = {Tprog}\n' + 
+                f'N буф = {Nbuf}\n' +
+                f'T буф = {Tbuf}')
     print('===')
-    print(f' p0 = {p0}\n' +
-    f'p1 = {p1}\n' + 
-    f'p2 = {p2}\n' +
-    f'p3 = {p3}\n' + 
-    f'p4 = {p4}\n' + 
-    f'p5 = {p5}\n' + 
-    f'sum P = {p1 + p2 + p3 + p4 + p5}\n' + 
-    f'Q = {Q}\n' + 
-    f'S = {S}\n' + 
-    f'P отк = {Potk}\n' + 
-    f'K = {K}\n' + 
-    f'N прог = {Nprog}\n' +
-    f'T прог = {Tprog}\n' + 
-    f'N буф = {Nbuf}\n' +
-    f'T буф = {Tbuf}')
+    print(outPutText)
+
+    root = Tk()
+    ex = Window(root)
+    #root.geometry("450x400+500+500")
+    root.mainloop()
     pass
